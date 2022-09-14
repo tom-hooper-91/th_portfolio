796d618ab96fb6e95876f08d2f13e75c53af82fa
Tom Hooper <tomwbhooper@hotmail.com>
2022-09-14 16:06:59 +1200

    configure es-lint

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants'
-import './Navbar.scss'
+import { images } from '../../constants';
+import './Navbar.scss';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const navItems = ['home', 'about', 'work', 'skills', 'contact'];
 
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo' />
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
       </div>
-      <ul className='app__navbar-links'>
-        {navItems.map(item => (
-          <li key={`link-${item}`} className='app__flex p-text'>
+      <ul className="app__navbar-links">
+        {navItems.map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
-      
-      <div className='app__navbar-menu'>
+
+      <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
           <motion.div
-            whileInView={{x: [300, 0]}}
+            whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
-            <ul className='app__navbar-links'>
-              {navItems.map(item => (
-                <li key={item} className='app__flex p-text'>
+            <ul className="app__navbar-links">
+              {navItems.map((item) => (
+                <li key={item} className="app__flex p-text">
                   <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                 </li>
               ))}
@@ -43,7 +43,7 @@ const Navbar = () => {
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
